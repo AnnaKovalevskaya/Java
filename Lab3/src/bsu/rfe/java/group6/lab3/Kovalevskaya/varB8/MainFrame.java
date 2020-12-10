bad62b5ba0191005985461342a96c5af36c76d39
Ann <annakovalevskaya19@gmail.com>
2020-12-10 15:59:50 +0300

    Second commit of Lab3

--- Lab3/src/bsu/rfe/java/group6/lab3/Kovalevskaya/varB8/MainFrame.java
+++ Lab3/src/bsu/rfe/java/group6/lab3/Kovalevskaya/varB8/MainFrame.java
@@ -29,7 +29,7 @@ import javax.swing.JTextField;
 @SuppressWarnings("serial")
 
 public class MainFrame extends JFrame {
-    private static final int WIDTH = 700;
+    private static final int WIDTH = 800;
     private static final int HEIGHT = 500;
  // Массив коэффициентов многочлена
     private Double[] coefficients;
